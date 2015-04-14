d4c97719e5ecb5f62312ca0409ebb0004c27c4f3
Christian Metzler <christianmetzler@web.de>
2015-04-14 10:17:21 +0200

    CCD1 Added calculation of length

--- Geometry.Line.pas
+++ Geometry.Line.pas
@@ -19,7 +19,7 @@ implementation
 
 function TLine.GetLength: Double;
 begin
-  {TODO: Implement}
+  Result := Sqrt(Sqr(StartPoint.GetX - EndPoint.GetX) + Sqr(StartPoint.GetY - EndPoint.GetY));
 end;
 
 end.
