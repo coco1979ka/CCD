bec1dd3ad0cc6fd2282cf3f6c396c8981dd694e6
Christian Metzler <christianmetzler@web.de>
2015-04-14 10:39:00 +0200

    Added caching of length

--- Geometry.Line.pas
+++ Geometry.Line.pas
@@ -7,19 +7,50 @@ uses
 
 type
   TLine = class
+  private
+    FChanged: Boolean;
+    FCachedLength: Double;
+    FStartPoint: TPoint;
+    FEndPoint: TPoint;
+    procedure SetEndPoint(const Value: TPoint);
+    procedure SetStartPoint(const Value: TPoint);
   public
-    StartPoint: TPoint;
-    EndPoint: TPoint;
+    constructor Create;
     function GetLength: Double;
+    property StartPoint: TPoint read FStartPoint write SetStartPoint;
+    property EndPoint: TPoint read FEndPoint write SetEndPoint;
   end;
 
 implementation
 
 { TLine }
 
+constructor TLine.Create;
+begin
+  FChanged := True;
+  FCachedLength := 0.0;
+end;
+
 function TLine.GetLength: Double;
 begin
-  Result := StartPoint.DistanceTo(EndPoint);
+  if FChanged then
+  begin
+    FCachedLength := StartPoint.DistanceTo(EndPoint);
+    FChanged := False;
+  end;
+  Result := FCachedLength;
+end;
+
+procedure TLine.SetEndPoint(const Value: TPoint);
+begin
+  FEndPoint := Value;
+  FChanged := True;
+end;
+
+procedure TLine.SetStartPoint(const Value: TPoint);
+begin
+  FStartPoint := Value;
+  FChanged := False;
 end;
 
 end.
