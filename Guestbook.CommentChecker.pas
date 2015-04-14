84ef52fcaf742a4e55817bf7e5f6bcbc2aa72828
Christian Metzler <christianmetzler@web.de>
2015-04-14 23:01:29 +0200

    CCD6 added ICheckedAspect interface and implementation for duplicated checks

--- Guestbook.CommentChecker.pas
+++ Guestbook.CommentChecker.pas
@@ -7,32 +7,78 @@ uses
   Guestbook.Comment;
 
 type
+  ICheckedAspect = interface
+  ['{B8570EB4-1B7F-45D0-AF94-F21D13B9D317}']
+    function IsPositiveFor(Comment: TComment): Boolean;
+  end;
+
   TCommentChecker = class
+  private
+    FChecks: TList<ICheckedAspect>;
+    FExistingComments: TList<TComment>;
+  public
+    constructor Create(ExistingComments: TList<TComment>);
+    destructor Destroy; override;
+    function IsValid(Comment: TComment): Boolean;
+  end;
+
+  TIsDuplicatedCheck = class(TInterfacedObject, ICheckedAspect)
   private
     FExistingComments: TList<TComment>;
   public
     constructor Create(ExistingComments: TList<TComment>);
-    function IsValid(Comment: TComment): Boolean;
+    function IsPositiveFor(Comment: TComment): Boolean;
   end;
 
 implementation
+
+var
+  Comments: TList<TComment>;
+  Aspects: TList<ICheckedAspect>;
 
 { TCommentChecker }
 
 constructor TCommentChecker.Create(ExistingComments: TList<TComment>);
 begin
+  FChecks := TList<ICheckedAspect>.Create;
+  FChecks.Add(TIsDuplicatedCheck.Create(ExistingComments));
+
   FExistingComments := ExistingComments;
 end;
 
-function TCommentChecker.IsValid(Comment: TComment): Boolean;
+destructor TCommentChecker.Destroy;
 begin
+  FChecks.Free;
+  inherited;
+end;
+
+function TCommentChecker.IsValid(Comment: TComment): Boolean;
+var
+  Check: ICheckedAspect;
+begin
+  for Check in FChecks do
+    if Check.IsPositiveFor(Comment) then Exit(False);
+
+
   if Comment.IsEmpty then
     Exit(False);
   if Comment.ContainsSpam then
     Exit(False);
-  if not Comment.IsUnicum(FExistingComments) then
-    Exit(False);
   Exit(True);
 end;
+
+{ TIsDuplicatedCheck }
+
+constructor TIsDuplicatedCheck.Create(ExistingComments: TList<TComment>);
+begin
+  FExistingComments := ExistingComments;
+end;
+
+function TIsDuplicatedCheck.IsPositiveFor(Comment: TComment): Boolean;
+begin
+  Result := not Comment.IsUnicum(FExistingComments);
+end;
+
+
 
 end.
